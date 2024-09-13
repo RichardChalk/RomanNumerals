f8d3bd7aa3c0f833fdfa431c3e4997ad0ff47b85
Richard Chalk <barmybrit@gmail.com>
2024-09-13 08:44:34 +0200

    Add project files.

---
+++ RomanNumerals/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace RomanNumerals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXTRA!
+            // Använd en klass med ett bibliotek!
+            // var translateRoman = new RomanNumeralsClass();
+            // translateRoman.GatherRomanInput();
+
+            // Använd endast Main - Inga övriga metoder
+            Console.WriteLine("Ange ett romerskt tal:");
+            string romanInput = Console.ReadLine().ToUpper();
+
+            int decimalOutput = 0;
+
+            for (int i = 0; i < romanInput.Length; i++)
+            {
+                int current = 0;
+
+                // Använd switch för att bestämma värdet av varje romersk siffra
+                switch (romanInput[i])
+                {
+                    case 'I':
+                        current = 1;
+                        break;
+                    case 'V':
+                        current = 5;
+                        break;
+                    case 'X':
+                        current = 10;
+                        break;
+                    case 'L':
+                        current = 50;
+                        break;
+                    case 'C':
+                        current = 100;
+                        break;
+                    case 'D':
+                        current = 500;
+                        break;
+                    case 'M':
+                        current = 1000;
+                        break;
+                    default:
+                        Console.WriteLine("Ogiltig romersk siffra");
+                        return;
+                }
+
+                // Kolla om nästa siffra är större, då ska vi subtrahera nuvarande
+                if (i + 1 < romanInput.Length)
+                {
+                    int next = 0;
+
+                    switch (romanInput[i + 1])
+                    {
+                        case 'I':
+                            next = 1;
+                            break;
+                        case 'V':
+                            next = 5;
+                            break;
+                        case 'X':
+                            next = 10;
+                            break;
+                        case 'L':
+                            next = 50;
+                            break;
+                        case 'C':
+                            next = 100;
+                            break;
+                        case 'D':
+                            next = 500;
+                            break;
+                        case 'M':
+                            next = 1000;
+                            break;
+                    }
+
+                    if (current < next)
+                    {
+                        decimalOutput -= current;
+                    }
+                    else
+                    {
+                        decimalOutput += current;
+                    }
+                }
+                else
+                {
+                    decimalOutput += current;
+                }
+            }
+
+            Console.WriteLine($"Det motsvarande heltalet är: {decimalOutput}");
+        }
+    }
+}
+
---
+++ RomanNumerals/RomanNumeralsClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    public class RomanNumeralsClass
+    {
+        public void GatherRomanInput()
+        {
+            Console.WriteLine("Ange ett romerskt tal:");
+            string roman = Console.ReadLine().ToUpper();
+            int result = RomanToInt(roman);
+            Console.WriteLine($"Det motsvarande heltalet är: {result}");
+        }
+        public int RomanToInt(string s)
+        {
+            Dictionary<char, int> romanMap = new Dictionary<char, int>
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+            int total = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i + 1 < s.Length && romanMap[s[i]] < romanMap[s[i + 1]])
+                {
+                    total -= romanMap[s[i]];
+                }
+                else
+                {
+                    total += romanMap[s[i]];
+                }
+            }
+            return total;
+        }
+    }
+}
